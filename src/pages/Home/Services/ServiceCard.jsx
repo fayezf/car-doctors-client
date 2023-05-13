9e416671ff888fe900c8fb8e98914ad4ff2ff379
fayezf <mohammadfayezahmed835@gmail.com>
2023-05-13 14:19:23 +0600

    all data done

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const ServiceCard = ({ service }) => {
-    const { title, img, price } = service;
+    const { _id, title, img, price } = service;
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
@@ -11,10 +11,10 @@ const ServiceCard = ({ service }) => {
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p className='text-xl text-orange-500'>Price: ${price}</p>
-                <div className="card-actions flex-grow">
-                    <button className="btn btn-circle">
-                        <FaArrowRight></FaArrowRight>
-                    </button>
+                <div className="card-actions">
+                    <Link to={`/checkout/${_id}`}>
+                        <button className='btn btn-primary'>Book Now</button>
+                    </Link>
                 </div>
             </div>
         </div>
